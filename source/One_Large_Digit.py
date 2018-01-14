f9544076106550c8b7c6e48e22efba6ea00ca642
aprilcoffee <8765498@gmail.com>
2018-01-15 03:18:32 +0800

    hello

--- source/One_Large_Digit.py
+++ source/One_Large_Digit.py
@@ -2,21 +2,60 @@ import RPi.GPIO as GPIO
 import time
 
 #GPIO declarations
-segmentClock = 23
-segmentLatch = 24
-segmentData = 25
+#RightUp - A
+segmentLatch1 = 23
+segmentClock1 = 24
+segmentData1 = 25
+
+#LeftUp - B
+segmentLatch2 = 17
+segmentClock2 = 27
+segmentData2 = 22
+
+#RightDown - C
+segmentLatch3 = 5
+segmentClock3 = 6
+segmentData3 = 13
+
+#LeftDown - D
+segmentLatch4 = 16
+segmentClock4 = 20
+segmentData4 = 21
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup(segmentClock,GPIO.OUT)
-GPIO.setup(segmentLatch,GPIO.OUT)
-GPIO.setup(segmentData,GPIO.OUT)
+GPIO.setup(segmentClock1,GPIO.OUT)
+GPIO.setup(segmentLatch1,GPIO.OUT)
+GPIO.setup(segmentData1,GPIO.OUT)
 
-GPIO.output(segmentClock,GPIO.LOW)
-GPIO.output(segmentLatch,GPIO.LOW)
-GPIO.output(segmentData,GPIO.LOW)
+GPIO.output(segmentClock1,GPIO.LOW)
+GPIO.output(segmentLatch1,GPIO.LOW)
+GPIO.output(segmentData1,GPIO.LOW)
 
+GPIO.setup(segmentClock2,GPIO.OUT)
+GPIO.setup(segmentLatch2,GPIO.OUT)
+GPIO.setup(segmentData2,GPIO.OUT)
+
+GPIO.output(segmentClock2,GPIO.LOW)
+GPIO.output(segmentLatch2,GPIO.LOW)
+GPIO.output(segmentData2,GPIO.LOW)
+
+GPIO.setup(segmentClock3,GPIO.OUT)
+GPIO.setup(segmentLatch3,GPIO.OUT)
+GPIO.setup(segmentData3,GPIO.OUT)
+
+GPIO.output(segmentClock3,GPIO.LOW)
+GPIO.output(segmentLatch3,GPIO.LOW)
+GPIO.output(segmentData3,GPIO.LOW)
+
+GPIO.setup(segmentClock4,GPIO.OUT)
+GPIO.setup(segmentLatch4,GPIO.OUT)
+GPIO.setup(segmentData4,GPIO.OUT)
+
+GPIO.output(segmentClock4,GPIO.LOW)
+GPIO.output(segmentLatch4,GPIO.LOW)
+GPIO.output(segmentData4,GPIO.LOW)
 
 def postNumber(nuber, decimal):
 	#    -  A
@@ -63,9 +102,22 @@ def postNumber(nuber, decimal):
 	#Clock these bits out to the drivers
 	n = 0
 	while(n < 8):
-		GPIO.output(segmentClock,GPIO.LOW)
-		GPIO.output(segmentData,segments & 1 << (7 - n))
-                GPIO.output(segmentClock,GPIO.HIGH)
+		GPIO.output(segmentClock1,GPIO.LOW)
+		GPIO.output(segmentData1,segments & 1 << (7 - n))
+                GPIO.output(segmentClock1,GPIO.HIGH)
+
+		GPIO.output(segmentClock2,GPIO.LOW)
+                GPIO.output(segmentData2,segments & 1 << (7 - n))
+                GPIO.output(segmentClock2,GPIO.HIGH)
+		
+		GPIO.output(segmentClock3,GPIO.LOW)
+		GPIO.output(segmentData3,segments & 1 << (7 - n))
+                GPIO.output(segmentClock3,GPIO.HIGH)
+		
+		GPIO.output(segmentClock4,GPIO.LOW)
+                GPIO.output(segmentData4,segments & 1 << (7 - n))
+                GPIO.output(segmentClock4,GPIO.HIGH)
+
                 n+=1
 x = 0
 while(True):
@@ -74,8 +126,14 @@ while(True):
         else:
                 postNumber(x, 0)
 	
-	GPIO.output(segmentLatch,GPIO.LOW)
-	GPIO.output(segmentLatch,GPIO.HIGH) #Register moves storage register on the rising edge of RCK
+	GPIO.output(segmentLatch1,GPIO.LOW)
+        GPIO.output(segmentLatch1,GPIO.HIGH)
+	GPIO.output(segmentLatch2,GPIO.LOW)
+        GPIO.output(segmentLatch2,GPIO.HIGH)
+	GPIO.output(segmentLatch3,GPIO.LOW)
+        GPIO.output(segmentLatch3,GPIO.HIGH)
+	GPIO.output(segmentLatch4,GPIO.LOW)
+	GPIO.output(segmentLatch4,GPIO.HIGH) #Register moves storage register on the rising edge of RCK
 	time.sleep(0.5)
 	
 	x+=1
