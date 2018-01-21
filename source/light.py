4b8623b57d62fa2391e3da88883075ee051c7109
aprilcoffee <8765498@gmail.com>
2018-01-21 21:34:37 +0800

    update

--- source/light.py
+++ source/light.py
@@ -3,7 +3,25 @@ import time
 import datetime
 from random import randint
 import sc
+import socket 
 
 sc.init()
+#s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#host = '10.254.29.136'
+#port = 8888
+#s.bind((host,port))
+#s.listen(5)
+#conn,addr = s.accept()
+
 while True:
+    """
+    data = conn.recv(1024)
+    if data:
+        print("Receive Data: %s" % str(data.decode('utf-8')))
+    else:
+        break
+    """
+
+    #sc.showTime()
     sc.showLeftToRight()
+conn.close()
