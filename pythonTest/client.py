3c656df57d14905a2ec951b0910290ce29114fe6
aprilcoffee <8765498@gmail.com>
2018-02-03 16:59:47 +0800

    server

---
+++ pythonTest/client.py
@@ -0,0 +1,11 @@
+import socket 
+import time
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host = '127.0.0.1'
+port = 1688
+
+s.connect((host,port))
+while True:
+	data = "hello Pi"
+	s.sendall(data.encode('utf-8'))
+	time.sleep(0.5)
