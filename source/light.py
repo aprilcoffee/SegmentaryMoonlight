c74dce2dd1620f267e958aa7955af9a79a8e515f
aprilcoffee <8765498@gmail.com>
2018-02-07 13:34:34 +0800

    audio

--- source/light.py
+++ source/light.py
@@ -4,20 +4,28 @@ import datetime
 from random import randint
 import sc
 import socket 
-
+import mu
 sc.init()
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-host = '127.0.0.1'
-port = 1234
+host = '10.1.1.13'
+port = 1688
 s.bind((host,port))
 s.listen(5)
-
 sc.reset(10,0)
 while True:
+    
+    if data:
+        recvdata = str(data.decode('utf-8'))
+        if recvdata == "end":
+            mu.showLeftToRight()
+            stringing = "B"
+            conn.send(stringing.encode('utf-8'))
+        else:
+            mu.rhythem()
 
-
-    sc.showLeftToRight()
+    else:
+        conn,addr = s.accept()
 
 conn.close()
